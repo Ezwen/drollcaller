7358d13b1031d95f707c649adfe79954f4c2f645
Ezwen <ezwen@enevned.fr>
2022-01-18 12:37:21 +0100

    Add uberjar + Dockerfile

--- src/main/java/rocknrollcaller/cli/Command.kt
+++ src/main/java/rocknrollcaller/cli/Command.kt
@@ -7,7 +7,6 @@ import rocknrollcaller.configuration.MonitorBuilder
 import rocknrollcaller.configuration.MonitoringConfiguration
 import rocknrollcaller.core.Constants
 import java.nio.file.Path
-import java.nio.file.Paths
 import java.util.concurrent.Callable
 
 
@@ -23,17 +22,15 @@ class Command : Callable<Int> {
         arity = "1..1",
         description = ["The YAML monitoring configuration file."]
     )
-    var configurationFilePath: Path = Paths.get("./monitoring-configuration.yml")
+    var configurationFilePath: Path? = null
 
     override fun call(): Int {
         val mapper = YAMLMapper.builder().enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_VALUES).build()
-        val confPath = configurationFilePath.toFile()
+        val confPath = configurationFilePath!!.toFile()
         val confParsed = mapper.readValue(confPath, MonitoringConfiguration::class.java)
         val builder = MonitorBuilder()
         val monitor = builder.createMonitorFromConfiguration(confParsed)
         monitor.start()
         return 0
     }
-
-
 }
