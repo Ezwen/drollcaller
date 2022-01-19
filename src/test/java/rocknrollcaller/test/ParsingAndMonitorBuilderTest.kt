dfb7eb3f3d95274a3e2c43ffc8d49e6bdb564524
Ezwen <ezwen@enevned.fr>
2022-01-19 11:30:04 +0100

    Make test cases use main example conf

--- src/test/java/rocknrollcaller/test/ParsingAndMonitorBuilderTest.kt
+++ src/test/java/rocknrollcaller/test/ParsingAndMonitorBuilderTest.kt
@@ -5,47 +5,45 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
 import org.junit.jupiter.api.Test
 import rocknrollcaller.configuration.MonitorBuilder
 import rocknrollcaller.configuration.MonitoringConfiguration
+import java.nio.file.Files
+import java.nio.file.Paths
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 class ParsingAndMonitorBuilderTest {
     private fun parse(path: String): MonitoringConfiguration? {
         val mapper = YAMLMapper.builder().enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_VALUES).build()
-        val yamlContent: String = getResourceContent(path)
+        val yamlContent: String =  Files.readString(Paths.get(path))
         return mapper.readValue(yamlContent, MonitoringConfiguration::class.java)
     }
 
-    private fun getResourceContent(path: String): String {
-        return ParsingAndMonitorBuilderTest::class.java.getResource(path).readText()
-    }
-
     @Test
-    fun testParsingDummy() {
-        val result = parse("../../dummyMonitoringConfiguration.yml")
+    fun testParsingMainExample() {
+        val result = parse("examples/example_monitoring_configuration.yml")
         assertNotNull(result)
         assertEquals("Asia/Tokyo", result.timezone)
         assertEquals("Some monitoring", result.description)
     }
 
     @Test
-    fun testBuilderDummy() {
+    fun testBuilderMainExample() {
         val builder = MonitorBuilder()
-        val monitor = builder.createMonitorFromConfiguration(parse("../../dummyMonitoringConfiguration.yml")!!)
+        val monitor = builder.createMonitorFromConfiguration(parse("examples/example_monitoring_configuration.yml")!!)
         assertNotNull(monitor)
     }
 
     @Test
-    fun testParsingNoSleep() {
-        val result = parse("../../noSleepConfiguration.yml")
+    fun testParsingNoOptionnals() {
+        val result = parse("src/test/resources/noOptionalsConfiguration.yml")
         assertNotNull(result)
         assertEquals("Asia/Tokyo", result.timezone)
         assertEquals("Some monitoring", result.description)
     }
 
     @Test
-    fun testBuilderNoSleep() {
+    fun testBuilderNoOptionnals() {
         val builder = MonitorBuilder()
-        val monitor = builder.createMonitorFromConfiguration(parse("../../noSleepConfiguration.yml")!!)
+        val monitor = builder.createMonitorFromConfiguration(parse("src/test/resources/noOptionalsConfiguration.yml")!!)
         assertNotNull(monitor)
     }
 
